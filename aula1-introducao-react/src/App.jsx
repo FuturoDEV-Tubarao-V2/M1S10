40b1fd16bdcc430210343b21f7cb7d158b7e57f6
Nicholas Macedo <33987691+nicholasmacedoo@users.noreply.github.com>
2024-04-11 22:22:59 -0300

    refactor: :truck: Arquivos desenvolvidos durante a aula de React

---
+++ aula1-introducao-react/src/App.jsx
@@ -0,0 +1,20 @@
+import Botao from './Botao'
+
+function App() {
+  return (
+    <div className="App">
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+      <Botao />
+    </div>
+  );
+}
+
+export default App;
---
+++ aula1-introducao-react/src/Botao.jsx
@@ -0,0 +1,23 @@
+//import React from 'react'
+
+function Botao() {
+    // Utilizando o fragment de dentro do react
+    // return (
+    //     <React.Fragment>
+    //         <strong>Titulo</strong>
+    //         <button>Click</button>
+    //     </React.Fragment>
+    // )
+    // Utilizando o fragment sem importação do react
+    // return (
+    //     <>
+    //         <strong>Titulo</strong>
+    //         <button>Click</button>
+    //     </>
+    // )
+    return (
+        <button>Click me</button>
+    )
+}
+
+export default Botao;
